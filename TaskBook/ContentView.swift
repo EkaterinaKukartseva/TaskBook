30558754b0df33123b785464161f4d21beb567c1
Екатерина <sos91195@mail.ru>
2022-12-18 20:07:52 +0300

    added project

---
+++ TaskBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskBook
+//
+//  Created by EKATERINA  KUKARTSEVA on 18.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskBook/TaskBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskBookApp.swift
+//  TaskBook
+//
+//  Created by EKATERINA  KUKARTSEVA on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
